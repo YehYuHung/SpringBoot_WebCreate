f70a437b9e6bbb0f7931e0c1e6e442ee40cf31a3
morgaYeh <z123z14795@gmail.com>
2022-02-14 17:38:54 +0800

    20220214 Thymeleaf模板使用/js/css (借用)使用成功

--- src/main/java/com/estherNmorga/demo/controll/MemberController.java
+++ src/main/java/com/estherNmorga/demo/controll/MemberController.java
@@ -1,14 +1,26 @@
 package com.estherNmorga.demo.controll;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
-@RestController
+import com.estherNmorga.demo.model.MemberModel;
+
+@Controller
 public class MemberController {
 
-	@GetMapping(value = "/")
-	public String loginWebPage() {
+	@GetMapping(value = "/welcome")
+	public String welcomeTest() {
 		return "Welcome";
+	}
+	
+	@GetMapping(value = {"/", "/login"})
+	public String login(@ModelAttribute MemberModel memberModel) {
+		return "login";
+	}
+	
+	@GetMapping(value = "/register")
+	public String register(@ModelAttribute MemberModel memberModel) {
+		return "register";
 	}
 }
