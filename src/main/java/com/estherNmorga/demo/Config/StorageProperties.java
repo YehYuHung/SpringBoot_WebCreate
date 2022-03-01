eb5f8ecf74b2e4069b7295b3a80890ead4d041a4
morgaYeh <z123z14795@gmail.com>
2022-03-01 16:41:27 +0800

    20220301 ConfigurationProperties外部加載檔案(路徑需在resource) 成功

--- src/main/java/com/estherNmorga/demo/Config/StorageProperties.java
+++ src/main/java/com/estherNmorga/demo/Config/StorageProperties.java
@@ -1,11 +1,24 @@
 package com.estherNmorga.demo.Config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
-@ConfigurationProperties("storage")
+// propertySource 可設定外部properties加載
+// 若無，則自動從application.properties搜索
+// 找不到不會報錯，只會將值賦予null的值
+@PropertySource("classpath:personal.properties")
+
+@ConfigurationProperties(prefix = "personal", ignoreUnknownFields = true)
+
+// 搭配 Configuration 加載，若無則需要使用 @EnableConfigurationProperties在Application.java上強制加載
+@Configuration
+
+// 注意！ 下方結構需對應properites檔，若無則找不到相對應結構資料
 public class StorageProperties {
 	
-	private String location = "C:\\temp\\Morga\\webCreate";
+	private String location;
+	private String password;
 
 	public String getLocation() {
 		return location;
@@ -14,4 +27,19 @@ public class StorageProperties {
 	public void setLocation(String location) {
 		this.location = location;
 	}
+
+	
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "{location=" + location + ", password=" + password + "}";
+	}
+	
 }
